188fe4543a0b1e33641f4b83a6d859b892856b14
Yaw Boakye <wheresyaw@gmail.com>
2017-02-06 10:12:44 +0000

    Prepare the handle saving draft posts

--- app/routes/write.js
+++ app/routes/write.js
@@ -1,8 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  resetController(controller, isExiting, transition) {
+    return this._super(...arguments);
+  },
+
   actions: {
     willTransition(transition) {
+      return this._super(...arguments);
     }
   }
 });
